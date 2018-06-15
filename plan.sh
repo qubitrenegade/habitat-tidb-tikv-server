858f7b884f527d3bc32c02caae2907f69af06978
qubitrenegade <qubitrenegade@gmail.com>
2018-06-15 00:37:11 -0600

    updated things, did stuff... basically at this point it seems to run, but errs with max num file descriptors.  Going to try in container see if that helps...

--- plan.sh
+++ plan.sh
@@ -3,8 +3,8 @@ pkg_origin=qubitrenegade
 pkg_version=2.0.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MPL-2.0")
-pkg_source=http://download.pingcap.org/tidb-latest-linux-amd64.tar.gz
-pkg_shasum=1c58d4837a0e6da6a034e8ff48da073f8119bd3c9bf3dc56442a42aed4978c6c
+pkg_source=http://download.pingcap.org/tidb-v${pkg_version}-linux-amd64-unportable.tar.gz
+pkg_shasum=be725a370345504ed9dc495ec3bec1876b099eedf29486afe2e3da61b2bea038
 pkg_deps=(core/bash core/glibc core/gcc-libs)
 pkg_build_deps=(core/patchelf)
 pkg_bin_dirs=(bin)
@@ -47,13 +47,13 @@ pkg_upstream_url="https://github.com/pingcap/tikv"
 
 do_clean() {
   # remove any old binaries
-  debug $(rm -rf ${HAB_CACHE_SRC_PATH}/tidb-latest-linux-amd64/)
+  build_line $(rm -vrf ${HAB_CACHE_SRC_PATH}/tidb-v${pkg_version}-linux-amd64/)
   do_default_clean
 }
 
 do_prepare() {
   # the binaries are distributed together.  We'll separate them out now
-  debug $(cp -v ${HAB_CACHE_SRC_PATH}/tidb-latest-linux-amd64/bin/${pkg_name}* \
+  build_line $(cp -v ${HAB_CACHE_SRC_PATH}/tidb-v${pkg_version}-linux-amd64-unportable/bin/${pkg_name}* \
     ${HAB_CACHE_SRC_PATH}/$pkg_dirname)
 }
 
@@ -61,9 +61,9 @@ do_build() {
   # Update our interpreter and add link to gcc shared object.
   # AKA patchelf black magic
   for i in ./${pkg_name}*; do
-    debug "patching: patchelf --interpreter $(pkg_path_for glibc)/lib64/ld-linux-x86-64.so.2 ${i}"
+    build_line "patching: patchelf --interpreter $(pkg_path_for glibc)/lib64/ld-linux-x86-64.so.2 ${i}"
     patchelf --interpreter "$(pkg_path_for glibc)/lib64/ld-linux-x86-64.so.2" ${i}
-    debug "adding libgcc_s.so.1: patchelf --add-needed $(pkg_path_for gcc-libs)/lib/libgcc_s.so.1 ${i}"
+    build_line "adding libgcc_s.so.1: patchelf --add-needed $(pkg_path_for gcc-libs)/lib/libgcc_s.so.1 ${i}"
     patchelf --add-needed "$(pkg_path_for gcc-libs)/lib/libgcc_s.so.1" ${i}
   done
 }
